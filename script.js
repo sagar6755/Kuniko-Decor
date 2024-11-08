50dac24cd342bd2904f4558b6b9774d55468819c
Sanjay kumar <hrsanjaysagar96@gmail.com>
2024-11-08 17:01:38 +0530

    responsive nav

--- script.js
+++ script.js
@@ -6,3 +6,30 @@ function showSidebar(){
 function hideSideBar(){
     sideBar.style.display = 'none'
 }
+
+function showSearchBar(){
+    const searchBar = document.querySelector('.search-bar')
+    searchBar.style.display = 'flex'
+
+}
+function hideSearchBar(){
+    const searchBar = document.querySelector('.search-bar')
+ 
+    searchBar.style.display = 'none'
+}
+
+function searchBarIcon(){
+    const searchBar = document.querySelector('.search-bar')
+
+    
+    if (searchBar.style.display === 'none'|| searchBar.style.display === ''){
+
+        showSearchBar();
+    }
+    
+    else if (searchBar.style.display === 'flex') {
+        hideSearchBar();
+        
+    }
+}
+
