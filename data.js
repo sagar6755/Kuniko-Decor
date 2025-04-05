a17fb5684f09921a7d8d57baccb9843ccfad4dc6
Sanjay kumar <hrsanjaysagar96@gmail.com>
2025-04-05 08:36:11 +0530

    final changes

--- data.js
+++ data.js
@@ -1,144 +1,133 @@
-{/* <div class="cards">       
-                <div class="card">
-                    <img class="card-img" src="images/4.jpg" alt="toran">
-                    <div class="card-details">
-                        <h3>Title: Toran</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempore, </p>
-                        <button class="add-btn btn">Add to cart</button>
-                        <button class="buy-btn btn">buy now</button>
-                    </div>
-                    </div> 
-                    </div>*/}
 
-class Data {
+export default class Data {
     // constructor(parameters) {
-        
+
     // }
 
     toran = [
-       
+
         {
-            title : "Exquisite Handcrafted Toran Garland | Door Hanging Toran",
+            title: "Exquisite Handcrafted Toran Garland | Door Hanging Toran",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/71+gjP0ualL._SL1500_.jpg",
+            image: "https://m.media-amazon.com/images/I/71+gjP0ualL._SL1500_.jpg",
             color: "Mango - Orange"
         },
         {
-            title : "Kunika Handicraft Decoration Handmade Marigold Hanging Toran | Decor for Door",
+            title: "Kunika Handicraft Decoration Handmade Marigold Hanging Toran | Decor for Door",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/815CseqRuoL._SL1500_.jpg",
+            image: "https://m.media-amazon.com/images/I/815CseqRuoL._SL1500_.jpg",
             color: "orange"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Flowers Garlands with Good Looking Latkan Made by Pompom and Bells for Decoration",
+            title: "Flowers Garlands with Good Looking Latkan Made by Pompom and Bells for Decoration",
             details: "Multi-Colour, Appx. Length 3ft.) (Pack of 6)",
-            image : "https://m.media-amazon.com/images/I/91CIaKU65mL._SL1500_.jpg",
-            color : "Mango Lemon"
+            image: "https://m.media-amazon.com/images/I/91CIaKU65mL._SL1500_.jpg",
+            color: "Mango Lemon"
 
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/610m3vOCYDL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/610m3vOCYDL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/71L3DtciWKL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/71L3DtciWKL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/71qM+jNhyUL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/71qM+jNhyUL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/61zXGuLa4nL.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/61zXGuLa4nL.jpg",
+            color: "Orange & Mango"
         },
-        
+
         {
-            title : "Exquisite Handcrafted Toran Garland | Door Hanging Toran",
+            title: "Exquisite Handcrafted Toran Garland | Door Hanging Toran",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/71+gjP0ualL._SL1500_.jpg",
+            image: "https://m.media-amazon.com/images/I/71+gjP0ualL._SL1500_.jpg",
             color: "Mango - Orange"
         },
         {
-            title : "Kunika Handicraft Decoration Handmade Marigold Hanging Toran | Decor for Door",
+            title: "Kunika Handicraft Decoration Handmade Marigold Hanging Toran | Decor for Door",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/815CseqRuoL._SL1500_.jpg",
+            image: "https://m.media-amazon.com/images/I/815CseqRuoL._SL1500_.jpg",
             color: "orange"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Flowers Garlands with Good Looking Latkan Made by Pompom and Bells for Decoration",
+            title: "Flowers Garlands with Good Looking Latkan Made by Pompom and Bells for Decoration",
             details: "Multi-Colour, Appx. Length 3ft.) (Pack of 6)",
-            image : "https://m.media-amazon.com/images/I/91CIaKU65mL._SL1500_.jpg",
-            color : "Mango Lemon"
+            image: "https://m.media-amazon.com/images/I/91CIaKU65mL._SL1500_.jpg",
+            color: "Mango Lemon"
 
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/81GBvZGGfDL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/610m3vOCYDL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/610m3vOCYDL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/71L3DtciWKL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/71L3DtciWKL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/71qM+jNhyUL._SL1500_.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/71qM+jNhyUL._SL1500_.jpg",
+            color: "Orange & Mango"
         },
         {
-            title : "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
+            title: "Mandir Diwali Ganesha Chathurthi Other Festival |Festival Items | Door Hanging",
             details: "Multi color - Length 3.4ft",
-            image : "https://m.media-amazon.com/images/I/61zXGuLa4nL.jpg",
-            color : "Orange & Mango"
+            image: "https://m.media-amazon.com/images/I/61zXGuLa4nL.jpg",
+            color: "Orange & Mango"
         },
     ]
 }
 
 let toran = new Data;
- let response = JSON.stringify(toran.toran);
-let data =  JSON.parse(response);
-
-for(const elements of data){
+let response = JSON.stringify(toran.toran);
+let data = JSON.parse(response);
+console.log(data)
+for (const elements of data) {
     const card = document.createElement('div');
     card.className = 'card';
-    
+
     card.innerHTML = `<img class="card-img" src="${elements.image}" alt="toran">
                     <div class="card-body">
                         <h3>${elements.title}</h3>
@@ -146,11 +135,52 @@ for(const elements of data){
                         <p>${elements.details}</p>
                     </div>
                     <div class="btn-div">
-                        <button class="add-btn btn">Add to cart</button>
+                        <button onclick="addCart()" class="add-btn btn">Add to cart</button>
                     <button class="buy-btn btn">buy now</button>
                     </div>`
-    
+
     document.querySelector('.cards').append(card);
 }
- 
- 
+
+
+const searchInput = document.getElementById("searchInput");
+const suggestionsBox = document.getElementById("suggestions");
+
+function search()
+{
+searchInput.addEventListener("input", function () {
+    const searchValue = this.value.toLowerCase();
+    suggestionsBox.innerHTML = "";
+
+    if (searchValue) {
+        const filteredItems = data.filter(item => item.title.toLowerCase().includes(searchValue));
+        if (filteredItems.length > 0) {
+            suggestionsBox.style.display = "block";
+            filteredItems.forEach(item => {
+                const div = document.createElement("div");
+                div.textContent = item.title;
+                div.addEventListener("click", () => {
+                    searchInput.value = item.title;
+                    suggestionsBox.style.display = "none";
+                    console.log(item)
+                    data = item
+
+                });
+                suggestionsBox.appendChild(div);
+            });
+        } else {
+            suggestionsBox.style.display = "none";
+        }
+    } else {
+        suggestionsBox.style.display = "none";
+    }
+});
+}
+
+search();
+
+document.addEventListener("click", function (e) {
+    if (!searchInput.contains(e.target) && !suggestionsBox.contains(e.target)) {
+        suggestionsBox.style.display = "none";
+    }
+});
--- script.js
+++ script.js
@@ -1,82 +1,48 @@
 "use strict"
-//  import Data from './data.js';
 
-function showSidebar(){
+
+function showSidebar() {
     const sideBar = document.querySelector('aside')
     sideBar.style.display = 'flex'
     const body = document.querySelector('body');
     body.style.overflow = 'hidden';
-}
+};
 
-function hideSideBar(){
+function hideSideBar() {
     sideBar.style.display = 'none'
     const body = document.querySelector('body');
     body.style.overflow = 'scroll';
-}
+};
 
-function showSearchBar(){
+function showSearchBar() {
     const searchBar = document.querySelector('.search-bar')
     searchBar.style.display = 'flex'
 
 }
-function hideSearchBar(){
+function hideSearchBar() {
     const searchBar = document.querySelector('.search-bar')
- 
+
     searchBar.style.display = 'none'
 }
 
-function searchBarIcon(){
+function searchBarIcon() {
     const searchBar = document.querySelector('.search-bar')
 
-    
-    if (searchBar.style.display === 'none'|| searchBar.style.display === ''){
+
+    if (searchBar.style.display === 'none' || searchBar.style.display === '') {
 
         showSearchBar();
     }
-    
+
     else if (searchBar.style.display === 'flex') {
         hideSearchBar();
-        
+
     }
 }
 
-// autocomplete search bar
 
-const searchInput = document.getElementById("searchInput");
-const suggestionsBox = document.getElementById("suggestions");
 
-// Sample data for autocomplete
-const items = ["Toran", "Decoration Items", "Hanging items", "wall Decoration", "Door decoration", "Home decoration", "Mango", "Orange", "Peach", "Strawberry", "Watermelon"];
 
-searchInput.addEventListener("input", function() {
-    const searchValue = this.value.toLowerCase();
-    suggestionsBox.innerHTML = "";
-    
-    if (searchValue) {
-        const filteredItems = items.filter(item => item.toLowerCase().startsWith(searchValue));
-
-        if (filteredItems.length > 0) {
-            suggestionsBox.style.display = "block";
-            filteredItems.forEach(item => {
-                const div = document.createElement("div");
-                div.textContent = item;
-                div.addEventListener("click", () => {
-                    searchInput.value = item;
-                    suggestionsBox.style.display = "none";
-                });
-                suggestionsBox.appendChild(div);
-            });
-        } else {
-            suggestionsBox.style.display = "none";
-        }
-    } else {
-        suggestionsBox.style.display = "none";
-    }
-});
-
-// Hide suggestions when clicking outside
-document.addEventListener("click", function(e) {
-    if (!searchInput.contains(e.target) && !suggestionsBox.contains(e.target)) {
-        suggestionsBox.style.display = "none";
-    }
-});
+function addCart() {
+    alert('Added Successfully')
+}
